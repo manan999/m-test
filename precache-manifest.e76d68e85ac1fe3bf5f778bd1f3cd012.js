42ceb8510c484e2df203f4430e1ebd286e9c11a4
manan999 <aggarwalmanan4@gmial.com>
2019-04-13 17:02:20 +0530

    Trial, Added Evrything

---
+++ precache-manifest.e76d68e85ac1fe3bf5f778bd1f3cd012.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c367315c142425daab8",
+    "url": "/static/css/main.f9a8b0ad.chunk.css"
+  },
+  {
+    "revision": "4c367315c142425daab8",
+    "url": "/static/js/main.4c367315.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1bdeac8e0476b09690f5",
+    "url": "/static/css/2.67568b72.chunk.css"
+  },
+  {
+    "revision": "1bdeac8e0476b09690f5",
+    "url": "/static/js/2.1bdeac8e.chunk.js"
+  },
+  {
+    "revision": "92c9e72f93cc0f47fce2084bbfd1b0fc",
+    "url": "/static/media/ethnocentricrg.92c9e72f.woff"
+  },
+  {
+    "revision": "794d0d781b6d7f719e84ca6537fa6d16",
+    "url": "/static/media/m1.794d0d78.png"
+  },
+  {
+    "revision": "18c6db6cdd11eceffe5dc815affa87d0",
+    "url": "/static/media/m2.18c6db6c.png"
+  },
+  {
+    "revision": "a904e2155ea349140ce87c318d1a2b7f",
+    "url": "/static/media/m3.a904e215.png"
+  },
+  {
+    "revision": "7c8f940be0b8e601219c56f16f364dca",
+    "url": "/static/media/404.7c8f940b.png"
+  },
+  {
+    "revision": "66f8200c6577b104d71157c51c19103a",
+    "url": "/static/media/marvelbg.66f8200c.jpg"
+  },
+  {
+    "revision": "066670478e1e1f8553c37ca84d11d209",
+    "url": "/static/media/mobilebg.06667047.jpg"
+  },
+  {
+    "revision": "01af439d256c20a7e6abe63bece0b150",
+    "url": "/static/media/BEBAS.01af439d.woff"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fc044818f5ae2f33823db5403c5ed0da",
+    "url": "/index.html"
+  }
+];
